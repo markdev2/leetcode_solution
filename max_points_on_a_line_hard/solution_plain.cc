c474322036036e2f3992982e583b2d70dec062aa
wuxinwei <wcgwuxinwei@gmail.com>
2015-08-10 05:05:40 +0800

    create two new dir for hard

---
+++ max_points_on_a_line_hard/solution_plain.cc
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: solution_plain.cc
+	> Author: MarkWoo
+	> Mail:wcgwuxinwei@gmail.com 
+	> Created Time: Mon 10 Aug 2015 05:05:10 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+
---
+++ valid_number_hard/solution_plain.cc
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: solution_plain.cc
+	> Author: MarkWoo
+	> Mail:wcgwuxinwei@gmail.com 
+	> Created Time: Mon 10 Aug 2015 05:01:13 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+
+class Solution {
+	public:
+		bool isNumber(std::string &s) {
+			return false;
+		}
+};
+
+int main(void) {
+	return 0;
+}
