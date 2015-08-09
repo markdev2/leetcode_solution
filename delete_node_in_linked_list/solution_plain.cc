1d02f3850a93c05a2f377b3d3458d86dcdeecd1d
wuxinwei <wcgwuxinwei@gmail.com>
2015-08-10 03:45:29 +0800

    finish delete_node_in_linked_list

--- delete_node_in_linked_list/solution_plain.cc
+++ delete_node_in_linked_list/solution_plain.cc
@@ -25,67 +25,7 @@ struct ListNode {
 class Solution {
 	public:
     void DeleteNode(ListNode* node) {
-			if (NULL == node || NULL == node->next) {
-				return ;
-			}
-
-			int val = node->val;
-			ListNode *pointer = node;
-
-			if (NULL == pointer->next->next && 
-					NULL != pointer->next) {
-				if (pointer->next->val == val) {
-					ListNode *delete_node = pointer->next;
-					pointer->next = pointer->next->next;
-					delete delete_node;
-					delete_node = NULL;
-					return ;
-				}
-			}
-
-			while (NULL != pointer->next->next) {
-				if (pointer->next->val == val) {
-					std::cout << pointer->next->val << "********" << std::endl;
-					ListNode *delete_node = pointer->next;
-					pointer->next = pointer->next->next;
-					delete delete_node;
-					delete_node = NULL;
-				}
-				pointer = pointer->next;
-			}
+			node->val = node->next->val;
+			node->next = node->next->next;
 	  }
 };
-
-int main(void) {
-	Solution s1;
-	ListNode* node_header = new ListNode(3);
-	ListNode* ptr = node_header;
-
-	for (int i = 0; i < 10; ++i) {
-		ListNode* node = new ListNode(i);
-		ptr->next = node;
-		ptr = ptr->next;
-		//std::cout << node->val << " ";
-	}
-	std::cout << std::endl;
-
-	std::cout << "before : " << std::endl;
-	ptr = node_header;
-	while (ptr->next) {
-		std::cout << ptr->val << " ";
-		ptr = ptr->next;
-	}
-	std::cout << std::endl;
-
-	s1.DeleteNode(node_header);
-
-	std::cout << "after : " << std::endl;
-	ptr = node_header;
-	while (ptr->next) {
-		std::cout << ptr->val << " ";
-		ptr = ptr->next;
-	}
-	std::cout << std::endl;
-
-	return 0;
-}
