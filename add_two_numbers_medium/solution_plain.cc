dad27d4e732a534bf64d3c94bc1f38873e5607f8
Mark Woo <wcgwuxinwei@gmail.com>
2015-08-24 02:37:10 -0700

    update add two numbers

---
+++ add_two_numbers_medium/solution_plain.cc
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: solution_plain.cc
+	> Author: MarkWoo
+	> Mail:wcgwuxinwei@gmail.com 
+	> Created Time: 2015年08月24日 星期一 02时36分05秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
