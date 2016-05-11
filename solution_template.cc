cbd603504cf7e3aebfd8f16a814628a81db62031
markwoo <wcgwuxinwei@gmail.com>
2016-05-11 21:42:27 +0800

    update

---
+++ solution_template.cc
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: solution_template.cc
+	> Author: MarkWoo
+	> Mail:wcgwuxinwei@gmail.com 
+	> Created Time: Tue 10 May 2016 11:21:46 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+	public:
+		void someThing(void) {
+
+		}
+};
+
+int main(void)
+{
+	Solution solution;
+
+	return 0;
+}
